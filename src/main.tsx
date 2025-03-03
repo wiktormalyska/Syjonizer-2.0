0274424583981e7f34c529f3ffd52c7e95ff561e
Wiktor Małyska <wikimal200310@gmail.com>
2025-03-03 23:21:47 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+
+import {createRoot} from "react-dom/client";
+import {PopupComponent} from "./popup/PopupComponent.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <PopupComponent />
+)
---
+++ src/popup/PopupComponent.tsx
@@ -0,0 +1,8 @@
+export const PopupComponent = () => {
+    return (
+        <>
+            <div>Syjonizer 2.0</div>
+            <div>by Wiktor Małyska</div>
+        </>
+    )
+}
